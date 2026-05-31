e696d3ecffae387562ccac7bc79995b4b72f4a80
Brent <brent.cook1@pm.me>
2026-05-31 17:42:34 +1000

    B: Added additional methods and created the player.py and circleshape.py to the dir

--- main.py
+++ main.py
@@ -1,19 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-def main():
-    while True:
-        log_state()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
-        screen.fill("black")
-        pygame.display.flip()
-
-
+from player import Player
 
 
 
@@ -22,6 +10,32 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    dt = 0.0
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        #print (dt)
+
+def update(self, dt: float) -> None:
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_a]:
+        # ?
+    if keys[pygame.K_d]:
+        # ?
 
 if __name__ == "__main__":
     main()

ca3b67730377dab5f9c79652fbe6b563f1e40cc4
Brent <brent.cook1@pm.me>
2026-06-01 13:24:21 +1000

    C: Added functionality for player rotation.

--- main.py
+++ main.py
@@ -27,15 +27,10 @@ def main():
         player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+        player.update(dt)
         #print (dt)
 
-def update(self, dt: float) -> None:
-    keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_a]:
-        # ?
-    if keys[pygame.K_d]:
-        # ?
 
 if __name__ == "__main__":
     main()

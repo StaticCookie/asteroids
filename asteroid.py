d1ba8c290d736aeda1fa3c3ae2615079ae3c9db8
Brent <brent.cook1@pm.me>
2026-06-04 21:04:54 +1000

    E: Added collisons between asteroids and player's ship

---
+++ asteroid.py
@@ -0,0 +1,14 @@
+import pygame
+from constants import LINE_WIDTH
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x: float, y: float, radius: float) -> None:
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (self.position.x, self.position.y), self.radius,LINE_WIDTH)
+
+    def update(self, dt: float) -> None:
+        self.position += self.velocity * dt
+
--- main.py
+++ main.py
@@ -1,35 +1,56 @@
+import sys
+
 import pygame
+
+from asteroid import Asteroid
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
-
-
+from asteroidfield import AsteroidField
+from logger import log_event
 
 def main():
+    pygame.init()
+
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids,updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
+
+
+    asteroid_field = AsteroidField()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
 
     dt = 0.0
 
     while True:
+        for obj in asteroids:
+            if obj.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        player.update(dt)
-        #print (dt)
-
 
 
 if __name__ == "__main__":
